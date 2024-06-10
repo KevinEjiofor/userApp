1b588def896c204757a6670367e34bbcfe0f47b0
KevinEjiofor <ejioforkelvin@gmail.com>
2024-06-10 10:50:32 +0100

    Initial commit

---
+++ src/main/java/com.example.userApp/userAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.userApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class userAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(userAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com.example.userApp/userAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class userAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
